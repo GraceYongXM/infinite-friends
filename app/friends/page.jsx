465dc30730bb4c90deeb84401c4e471b9ff3b8f6
Grace Yong <grace.yong.xue.min@gmail.com>
2023-05-22 00:03:35 +0800

    filter icon

--- app/friends/page.jsx
+++ app/friends/page.jsx
@@ -1,13 +1,25 @@
+import React from "react";
+
+import Image from "next/image";
+
 import Header from "@component/components/Header";
 import Sidebar from "@component/components/Sidebar";
-import React from "react";
+import FilterIcon from "../../assets/filter.svg";
 
 const FriendPage = () => {
   return (
     <>
       <Sidebar page="friends"/>
       <Header page="Friends"/>
-      <div>FriendPage</div>
+      <div className="friends">
+        <button className="filter-button">
+            <Image src={FilterIcon} alt="Filter Icon" />
+        </button>
+
+        <div>
+            list of friends
+        </div>
+      </div>
     </>
   );
 };
