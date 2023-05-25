dd2e02316ede4eb923ffd741215f699f62645102
Grace Yong <grace.yong.xue.min@gmail.com>
2023-05-25 17:44:41 +0800

    make top headers sticky when scrolling

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,13 @@ import Sidebar from "@component/components/Sidebar";
 const Home = () => {
   return (
     <>
-      <Sidebar page="home"/>
-      <Header page="Home"/>
-      <div className="home">Welcome to the Clerkie Challenge!</div>
+      <Sidebar page="home" />
+      <div>
+        <Header page="Home" />
+        <div className="home">Welcome to the Clerkie Challenge!</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
